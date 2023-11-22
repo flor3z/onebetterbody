c4cd66c662bc8ec3f751d2ce8c3f509f97a740e0
Matheo <mflorez3@live.com>
2023-11-22 17:25:17 -0500

    styling adjustments for sign in and sign up pages

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -26,13 +26,13 @@ function Signin() {
 
   return (
     <div className="flex flex-col items-center justify-center bg-[#f2f2f7] h-screen">
-      <div className="max-w-lg px-5 py-16 text-center md:px-10 md:py-24 lg:py-32">
+      <div className="max-w-7xl px-5 py-16 text-center md:px-10 md:py-24 lg:py-32">
         <h2 className="mb-8 text-3xl font-bold md:mb-12 md:text-5xl">
           One Body
         </h2>
         <form
           onSubmit={handleSubmit}
-          className="mx-auto mb-4 max-w-sm pb-4"
+          className=" mb-4 max-w-sm w-80 pb-4"
           name="wf-form-password"
           method="get"
         >
@@ -73,19 +73,20 @@ function Signin() {
               </span>
             </div>
           ) : null}
-
-          <button className="flex items-center justify-center bg-[#276ef1] px-8 py-4 text-center font-semibold rounded-md text-white transition [box-shadow:rgb(171,_196,_245)_-8px_8px] hover:[box-shadow:rgb(171,_196,_245)_0px_0px]">
-            <p className="mr-6 font-bold">Sign in</p>
-            <svg
-              className="h-4 w-4 flex-none"
-              fill="currentColor"
-              viewBox="0 0 20 21"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Arrow Right</title>
-              <polygon points="16.172 9 10.101 2.929 11.515 1.515 20 10 19.293 10.707 11.515 18.485 10.101 17.071 16.172 11 0 11 0 9"></polygon>
-            </svg>
-          </button>
+          <div className="flex justify-center">
+            <button className="flex items-center justify-center bg-[#276ef1] px-8 py-4 text-center font-semibold rounded-md text-white transition [box-shadow:rgb(171,_196,_245)_-8px_8px] hover:[box-shadow:rgb(171,_196,_245)_0px_0px]">
+              <p className="mr-6 font-bold tracking-wider">Sign in</p>
+              <svg
+                className="h-4 w-4 flex-none"
+                fill="currentColor"
+                viewBox="0 0 20 21"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <title>Arrow Right</title>
+                <polygon points="16.172 9 10.101 2.929 11.515 1.515 20 10 19.293 10.707 11.515 18.485 10.101 17.071 16.172 11 0 11 0 9"></polygon>
+              </svg>
+            </button>
+          </div>
         </form>
         <p className="text-sm text-[#636262]">
           Dont have an account?{' '}
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -28,17 +28,17 @@ function Signup() {
   return (
     <>
       <section>
-        <div className="grid md:h-screen md:grid-cols-2">
+        <div className="grid h-screen md:h-screen md:grid-cols-2">
           <div className="flex flex-col items-center justify-center bg-white">
-            <div className="max-w-lg px-5 py-16 md:px-10 md:py-24 lg:py-32">
-              <div className="mb-6 ml-2 flex h-14 w-14 rounded-md items-center justify-center bg-[#276ef1] [box-shadow:rgb(171,_196,_245)_-8px_8px]">
+            <div className="max-w-lg px-5 py-5 md:px-10 md:py-24 lg:py-32">
+              <div className="mb-4 ml-2 flex h-8 w-8  sm:h-14 sm:w-14 rounded-md items-center justify-center bg-[#276ef1] [box-shadow:rgb(171,_196,_245)_-8px_8px]">
                 <img
                   src="https://assets.website-files.com/6357722e2a5f19121d37f84d/6358f5ec37c8c32b17d1c725_Vector-9.svg"
                   alt=""
-                  className="inline-block"
+                  className="inline-block h-5 w-5 sm:h-auto sm:w-auto"
                 />
               </div>
-              <p className="mb-8 text-[#647084] md:mb-12 lg:mb-16">
+              <p className="mb-6 text-[#647084] md:mb-12 lg:mb-16">
                 Stay connected with your account and check-in on your health
                 details. Keep in mind we have only one body and regular exercise
                 coupled with good nutrition leads to longevity.
@@ -48,13 +48,13 @@ function Signup() {
             </div>
           </div>
           <div className="flex flex-col items-center justify-center bg-[#f2f2f7]">
-            <div className="max-w-lg px-5 py-16 text-center md:px-10 md:py-24 lg:py-32">
-              <h2 className="mb-8 text-3xl font-bold md:mb-12 md:text-5xl">
+            <div className="max-w-lg px-5 py-12 text-center md:px-10 md:py-24 lg:py-32">
+              <h2 className="mb-8 text-4xl font-bold md:mb-12 md:text-5xl">
                 One Body
               </h2>
               <form
                 onSubmit={handleSubmit}
-                className="mx-auto mb-4 max-w-sm pb-4"
+                className="mx-auto w-80 mb-4 max-w-sm pb-4"
                 name="wf-form-password"
                 method="get"
               >
@@ -84,12 +84,12 @@ function Signup() {
                     onChange={(e) => setPassword(e.target.value)}
                     type="password"
                     className="mb-4 block h-9 w-full border border-black bg-[#f2f2f7] px-3 py-6 pl-14 text-sm text-[#333333]"
-                    placeholder="Password (min 4 characters)"
+                    placeholder="Password (min 6 characters)"
                     required=""
                   />
                 </div>
                 {error ? (
-                  <div className="flex items-center pb-4 font-medium">
+                  <div className="flex justify-center items-center pb-4 font-medium">
                     <span className=" inline-block cursor-pointer text-sm">
                       {error && (
                         <p className="text-red-500 font-bold">{error}</p>
@@ -97,18 +97,22 @@ function Signup() {
                     </span>
                   </div>
                 ) : null}
-                <button className="flex items-center justify-center bg-[#276ef1] px-8 py-4 text-center font-semibold rounded-md text-white transition [box-shadow:rgb(171,_196,_245)_-8px_8px] hover:[box-shadow:rgb(171,_196,_245)_0px_0px]">
-                  <p className="mr-6 font-bold">Create Account</p>
-                  <svg
-                    className="h-4 w-4 flex-none"
-                    fill="currentColor"
-                    viewBox="0 0 20 21"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <title>Arrow Right</title>
-                    <polygon points="16.172 9 10.101 2.929 11.515 1.515 20 10 19.293 10.707 11.515 18.485 10.101 17.071 16.172 11 0 11 0 9"></polygon>
-                  </svg>
-                </button>
+                <div className="flex justify-center">
+                  <button className="flex items-center justify-center bg-[#276ef1] px-8 py-4 text-center font-semibold rounded-md text-white transition [box-shadow:rgb(171,_196,_245)_-8px_8px] hover:[box-shadow:rgb(171,_196,_245)_0px_0px]">
+                    <p className="mr-6 font-bold tracking-wider">
+                      Create Account
+                    </p>
+                    <svg
+                      className="h-4 w-4 flex-none"
+                      fill="currentColor"
+                      viewBox="0 0 20 21"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <title>Arrow Right</title>
+                      <polygon points="16.172 9 10.101 2.929 11.515 1.515 20 10 19.293 10.707 11.515 18.485 10.101 17.071 16.172 11 0 11 0 9"></polygon>
+                    </svg>
+                  </button>
+                </div>
               </form>
               <p className="text-sm text-[#636262]">
                 Already have an account?{' '}
