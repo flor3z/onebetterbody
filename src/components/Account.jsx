a1b862311f4c3b32358e790e2627f6dd16ce321e
Matheo <mflorez3@live.com>
2023-12-07 21:42:11 -0500

    calculation for metric conversion and UI change using button state switch

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -7,6 +7,8 @@ function Account() {
   const [weight, setWeight] = useState('');
   const [height, setHeight] = useState('');
   const [result, setResult] = useState('');
+  const [inches, setInches] = useState('');
+  const [standardUnit, setStandardUnit] = useState(true);
   const [bmiInfo, setBmiInfo] = useState({});
 
   const { user, logout } = UserAuth();
@@ -22,12 +24,33 @@ function Account() {
     }
   };
 
+  const handleUnitSwitch = () => {
+    setStandardUnit(!standardUnit);
+  };
+
   const handleSubmitResult = (e) => {
     e.preventDefault();
-    const heightSquared = height * height;
-    setResult(weight / heightSquared);
-    setHeight('');
-    setWeight('');
+    if (standardUnit) {
+      const heightSquared = height * height;
+      setResult(weight / heightSquared);
+      setHeight('');
+      setWeight('');
+    } else {
+      const kilosIntoPounds = weight / 2.205;
+      const heightIntoMeters = height * 0.3048;
+      const heightTotal = heightIntoMeters + inches * 0.0254;
+      const metricHeightSquard = heightTotal * heightTotal;
+      console.log(
+        kilosIntoPounds,
+        heightIntoMeters,
+        heightTotal,
+        metricHeightSquard
+      );
+      setResult(kilosIntoPounds / metricHeightSquard);
+      setHeight('');
+      setInches('');
+      setWeight('');
+    }
   };
 
   const bmiResults = [
@@ -99,13 +122,13 @@ function Account() {
   return (
     <section>
       <div className="flex justify-between">
-        <div className="py-4 text-black font-semibold pl-5 sm:pl-10 underline decoration-[#276ef1] hover:decoration-[#277ef1] hover:decoration-2  underline-offset-2 ">
+        <div className="py-4 text-black font-bold pl-5 sm:pl-10 underline decoration-[#276ef1] hover:decoration-[#277ef1] hover:decoration-2  underline-offset-2 ">
           User: {user && user.email}
         </div>
         <div className="flex justify-center items-center py-4 pr-5 sm:pr-10">
           <button
             onClick={handleLogOut}
-            className="flex text-black items-center justify-center text-center font-semibold  underline decoration-[#276ef1] hover:decoration-[#277ef1] hover:decoration-2  underline-offset-2"
+            className="flex text-black items-center justify-center text-center underline decoration-[#276ef1] hover:decoration-[#277ef1] hover:decoration-2  underline-offset-2"
           >
             <p className="font-bold tracking-wider ">Sign Out</p>
           </button>
@@ -179,10 +202,18 @@ function Account() {
             </div>
             {/* </div> */}
           </div>
-          <div className="block w-auto h-auto md:h-full md:w-full overflow-hidden [grid-area:1/1/2/2] lg:[grid-area:1/1/2/2] rounded-xl border border-solid border-[#cdcdcd]">
+          <div className="relative block w-auto h-auto md:h-full md:w-full overflow-hidden [grid-area:1/1/2/2] lg:[grid-area:1/1/2/2] rounded-xl border border-solid border-[#cdcdcd]">
+            <button
+              onClick={handleUnitSwitch}
+              className="absolute top-3 right-4 font-bold underline decoration-[#276ef1] hover:decoration-[#277ef1] hover:decoration-2  underline-offset-2"
+            >
+              {standardUnit ? <span>Standard</span> : <span>Metric</span>}
+            </button>
             <form onSubmit={handleSubmitResult}>
-              <div className="flex flex-col items-center p-4 text-left ">
-                <label>Weight in Kilograms</label>
+              <div className="flex flex-col items-start p-4">
+                <label>
+                  {standardUnit ? 'Weight in Kilograms' : 'Weight in Pounds'}
+                </label>
                 <input
                   className="mb-4 block h-9 w-full border border-black bg-[#f2f2f7] px-3 py-6 pl-14 text-sm text-[#333333]"
                   onChange={(e) => setWeight(e.target.value)}
@@ -192,16 +223,45 @@ function Account() {
                   placeholder="input weight here"
                   required
                 />
-                <label>Height in Meters</label>
-                <input
-                  className=" mb-4 block h-9 w-full border border-black bg-[#f2f2f7] px-3 py-6 pl-14 text-sm text-[#333333]"
-                  onChange={(e) => setHeight(e.target.value)}
-                  value={height}
-                  step=".01"
-                  type="number"
-                  placeholder="input height here"
-                  required
-                />
+                <label>
+                  {standardUnit
+                    ? 'Height in Meters'
+                    : 'Height in Feet and Inches'}
+                </label>
+                {standardUnit ? (
+                  <input
+                    className=" mb-4 block h-9 w-full border border-black bg-[#f2f2f7] px-3 py-6 pl-14 text-sm text-[#333333]"
+                    onChange={(e) => setHeight(e.target.value)}
+                    value={height}
+                    step=".01"
+                    type="number"
+                    placeholder="input height here"
+                    required
+                  />
+                ) : (
+                  <>
+                    <input
+                      className=" mb-4 block h-9 w-full border border-black bg-[#f2f2f7] px-3 py-6 pl-14 text-sm text-[#333333]"
+                      onChange={(e) => setHeight(e.target.value)}
+                      value={height}
+                      maxLength="10"
+                      step=".01"
+                      type="number"
+                      placeholder="input feet here"
+                      required
+                    />
+                    <input
+                      className=" mb-4 block h-9 w-full border border-black bg-[#f2f2f7] px-3 py-6 pl-14 text-sm text-[#333333]"
+                      onChange={(e) => setInches(e.target.value)}
+                      value={inches}
+                      step=".01"
+                      type="number"
+                      placeholder="input inches here"
+                      required
+                    />
+                  </>
+                )}
+
                 <button
                   href="#"
                   className="flex items-center justify-center hover:bg-[#277ef1] bg-[#276ef1] px-8 py-4 text-center font-semibold rounded-md text-white"
